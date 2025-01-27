e394baf1e95ec328a7b3e89b0b0143de11e460ed
Derek Nutile <dereknutile@gmail.com>
2025-01-27 14:45:12 -0800

    Adding tags

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -16,9 +16,9 @@ export default {
         return {
             pageTitle: "Lesson 3 - Lists & Computed Properties",
             assignments: [
-                { id: 1, name: 'Finish Project', complete: false},
-                { id: 2, name: 'Read Chapter 4', complete: false},
-                { id: 3, name: 'Turn in Homework', complete: false},
+                { id: 1, name: 'Finish Project', complete: false, tag: 'math'},
+                { id: 2, name: 'Read Chapter 4', complete: false, tag: 'science'},
+                { id: 3, name: 'Turn in Homework', complete: false, tag: 'math'},
             ],
         }
     },
