96d127e77b935c915a34386eb2e4f1bb72c575e6
Derek Nutile <dereknutile@gmail.com>
2025-01-27 09:35:16 -0800

    Lesson 6

--- js/components/AppButton.js
+++ js/components/AppButton.js
@@ -1,16 +1,27 @@
 export default {
     template: `
         <button
-            class="bg-gray-200 hover:bg-gray-400 border rounded px-5 py-2 disabled:cursor-not-allowed"
+            :class="{
+                'border rounded px-5 py-2 disabled:cursor-not-allowed mr-1': true,
+                'bg-gray-200 hover:bg-gray-400': type === 'primary',
+                'border-none underline text-blue-500': type === 'cancel',
+                'bg-red-200 hover:bg-red-400': type === 'delete',
+                'is-loading': processing,
+            }"
             :disabled="processing"
             @click="processing = true"
         >
             <slot />
         </button>
     `,
-    data() {
-        return {
-            processing: false
-        };
+    props: {
+        type: {
+            type: String,
+            default: 'primary',
+        },
+        processing: {
+            type: Boolean,
+            default: false,
+        }
     }
 }
