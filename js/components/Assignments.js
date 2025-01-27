0bfd6374b3c71a38601599c9c64d91c89f2485e5
Derek Nutile <dereknutile@gmail.com>
2025-01-27 11:50:16 -0800

    Handle form submissions

--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -4,6 +4,12 @@ export default {
     template: `
         <assignment-list :assignments="filters.inProgress" title="Assignments In Progress"></assignment-list>
         <assignment-list :assignments="filters.completed" title="Completed Progress"></assignment-list>
+        <div class="border border-gray-600 text-black">
+            <form @submit.prevent="add">
+                <input v-model="newAssignment" placeholder="New Assignment" class="p-2">
+                <button class="bg-white p-2 border-l" type="submit">Add New</button>
+            </form>
+        </div>
     `,
     data() {
         return {
@@ -12,7 +18,8 @@ export default {
                 { id: 1, name: 'Finish Project', complete: false},
                 { id: 2, name: 'Read Chapter 4', complete: false},
                 { id: 3, name: 'Turn in Homework', complete: false},
-            ]
+            ],
+            newAssignment: '',
         }
     },
     computed: {
@@ -21,6 +28,16 @@ export default {
                 inProgress: this.assignments.filter(assignment => ! assignment.complete),
                 completed: this.assignments.filter(assignment => assignment.complete),
             }
+        }
+    },
+    methods: {
+        add() {
+            this.assignments.push({
+                name: this.newAssignment,
+                complete: false,
+                id: this.assignments.length + 1,
+            });
+            this.newAssignment = '';
         }
     }
 }
